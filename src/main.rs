f5b82ce6b16f11071b87675389aed42b21e74584
Dylan Green <cobaltgoldcsprinks@gmail.com>
2023-01-03 14:26:36 -0700

    Formatted Main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-use std::io::{stdout, Write, stdin};
 use colored::*;
-use cs_lib::{self, help_command};
 use cs_lib::rooms::Room;
+use cs_lib::{self, help_command};
+use std::io::{stdin, stdout, Write};
 
-fn main(){
+fn main() {
     let stdin = stdin();
     let room = Room::new();
 
@@ -15,10 +15,10 @@ fn main(){
 
         stdin.read_line(&mut input).expect("Failed to read line");
         input = (&mut input).trim().to_string();
-    
+
         match input.as_str() {
             "exit" => break,
-            "describe"=>println!("{}", room.describe()),
+            "describe" => println!("{}", room.describe()),
             "help" => help_command(),
             _ => println!("{}", "invalid command".red()),
         }
